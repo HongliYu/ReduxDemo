5c8166b47b5ff80008a2008fb81907ffdf239f64
HongliYu <yhlssdone@gmail.com>
2020-02-13 21:30:37 +0100

    Initial Commit

---
+++ ReduxDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ReduxDemo
+//
+//  Created by Hongli Yu on 2020/2/13.
+//  Copyright © 2020 Hongli Yu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
